51cf274eb9b440e8e97a869294e3b15ea7f8a3c2
Ankit Singh <ankitsingh10105@gmail.com>
2024-02-07 05:28:55 +0530

    first commit

--- script.js
+++ script.js
@@ -4,3 +4,22 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+  const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('show');
+      } else {
+        entry.target.classList.remove('show');
+      }
+    });
+  });
+
+  // Specify the elements you want to observe and animate
+  const animatedElements = document.querySelectorAll('.animated');
+
+  animatedElements.forEach(element => {
+    observer.observe(element);
+  });
+});
